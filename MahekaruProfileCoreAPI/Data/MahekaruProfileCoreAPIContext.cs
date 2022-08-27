04360c82e4720f360868fdcf2db48fb2671b1c35
Mahekaru <Mahekaru@gmail.com>
2022-08-27 02:17:42 -0400

    Created Models and scaffolding

---
+++ MahekaruProfileCoreAPI/Data/MahekaruProfileCoreAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MahekaruProfileCoreAPI.Models;
+
+namespace MahekaruProfileCoreAPI.Data
+{
+    public class MahekaruProfileCoreAPIContext : DbContext
+    {
+        public MahekaruProfileCoreAPIContext (DbContextOptions<MahekaruProfileCoreAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MahekaruProfileCoreAPI.Models.Profile> Profile { get; set; }
+    }
+}
---
+++ MahekaruProfileCoreAPI/Models/Profile.cs
@@ -0,0 +1,17 @@
+﻿namespace MahekaruProfileCoreAPI.Models
+{
+    public class Profile
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Exp { get; set; } = 0;
+        public int HP { get; set; }
+        public int MP { get; set; }
+        public string LinkedIn { get; set; }
+        public string Email { get; set; }
+        public string Title { get; set; }
+        public string GitHub { get; set; }
+        public string ProfileImage { get; set; }
+
+    }
+}
